4abc279b3abe437542b9cf0d470cc664c70094e9
DivyaSriThokala <divya.s.9595@gmail.com>
2024-03-22 03:52:19 +0530

    updated Base_url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -2,12 +2,21 @@ import axios from "axios";
 
 export const API_BASE_URL = "https://delicious-stamp-production.up.railway.app";
 
+// Create a new instance of Axios
+export const api = axios.create({ baseURL: API_BASE_URL });
 
-const jwt = localStorage.getItem("jwt");
-export const api = axios.create({
-    baseURL: API_BASE_URL,
-    headers: {
-        "Authorization": `Bearer ${jwt}`,
-        'Content-Type': "application/json"
+// Add a request interceptor to inject JWT token into the headers
+api.interceptors.request.use(config => {
+    // Retrieve JWT token from localStorage
+    const jwt = localStorage.getItem("jwt");
+
+    // If JWT token exists, set Authorization header
+    if (jwt) {
+        config.headers["Authorization"] = `Bearer ${jwt}`;
     }
-})
+
+    return config;
+}, error => {
+    // Handle request errors
+    return Promise.reject(error);
+});
