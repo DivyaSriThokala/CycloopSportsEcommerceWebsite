031118fcef2b2abb72acb7bec8fb4de96058c317
DivyaSriThokala <divya.s.9595@gmail.com>
2024-03-22 03:11:47 +0530

    updated Base_url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://delicious-stamp-production.up.railway.app/";
+export const API_BASE_URL = "https://delicious-stamp-production.up.railway.app";
 
 
 const jwt = localStorage.getItem("jwt");
